c1c3b5a6133271df3ff49bd1bbf1464140925e47
Ruijialu29 <lur3@student.unimelb.edu.au>
2024-09-08 04:32:02 +1000

    grasp

--- P1_Automail_Framework/src/Robot.java
+++ P1_Automail_Framework/src/Robot.java
@@ -91,29 +91,36 @@ public class Robot {
     }
 
     public void tick() {
-            Building building = Building.getBuilding();
             if (items.isEmpty()) {
-                // Return to MailRoom
-                if (room == building.NUMROOMS + 1) { // in right end column
-                    move(Building.Direction.DOWN);  //move towards mailroom
-                } else {
-                    move(Building.Direction.RIGHT); // move towards right end column
-                }
+                returnToMailRoom();
             } else {
                 // Items to deliver
-                if (floor == items.getFirst().myFloor()) {
-                    // On the right floor
-                    if (room == items.getFirst().myRoom()) { //then deliver all relevant items to that room
-                        do {
-                            Simulation.deliver(items.removeFirst());
-                        } while (!items.isEmpty() && room == items.getFirst().myRoom());
-                    } else {
-                        move(Building.Direction.RIGHT); // move towards next delivery
-                    }
-                } else {
-                    move(Building.Direction.UP); // move towards floor
-                }
+                deliverItems();
             }
+    }
+
+    public void returnToMailRoom(){
+        Building building = Building.getBuilding();
+        if (room == building.NUMROOMS + 1) { // in right end column
+            move(Building.Direction.DOWN);  //move towards mailroom
+        } else {
+            move(Building.Direction.RIGHT); // move towards right end column
+        }
+    }
+
+    public void deliverItems(){
+        if (floor == items.getFirst().myFloor()) {
+            // On the right floor
+            if (room == items.getFirst().myRoom()) { //then deliver all relevant items to that room
+                do {
+                    Simulation.deliver(items.removeFirst());
+                } while (!items.isEmpty() && room == items.getFirst().myRoom());
+            } else {
+                move(Building.Direction.RIGHT); // move towards next delivery
+            }
+        } else {
+            move(Building.Direction.UP); // move towards floor
+        }
     }
 
     public int numItems () {
